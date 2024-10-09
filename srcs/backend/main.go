ea0e378a3a74d34b472183f39d81cb4f29d58e05
hichame <hichome>
2024-10-10 01:58:46 +0200

    need test

--- srcs/backend/main.go
+++ srcs/backend/main.go
@@ -5,10 +5,8 @@ import (
 	"api/database"
 	"api/middleware"
 	"time"
-
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -18,7 +16,7 @@ var (
 	httpRequests = promauto.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "http_requests_total",
-			help: "Total number of HTTP request",
+			Help: "Total number of HTTP request",
 		},
 		[]string{"method", "endpoint", "status"},
 	)
@@ -32,15 +30,12 @@ var (
 )
 
 func prometheusMiddleware() gin.HandlerFunc {
-	return func(c *gin.Default) {
+	return func(c *gin.Context) {
 		start := time.Now()
-
 		c.Next()
-
 		duration := time.Since(start)
 		status := c.Writer.Status()
-
-		httpRequests.WithLabelValues(c.Request.Method, c.FullPath(), string(status)).Inc()
+		httpRequests.WithLabelValues(c.Request.Method, c.FullPath(), string(rune(status))).Inc()
 		httpDuration.WithLabelValues(c.Request.Method, c.FullPath()).Observe(duration.Seconds())
 	}
 }
@@ -48,7 +43,6 @@ func prometheusMiddleware() gin.HandlerFunc {
 func main() {
 	router := gin.Default()
 	database.New()
-
 	config := cors.Config{
 		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5173"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
@@ -56,21 +50,16 @@ func main() {
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	}
-
 	router.Use(cors.New(config))
 	router.Use(middleware.Token())
 	router.Use(prometheusMiddleware())
 	
-	router.GET("/metrics", gin.Wraph(promhttp.Handler()))
+	router.GET("/metrics", gin.WrapH(promhttp.Handler()))
 	router.Static("/users/avatar", "./avatars")
-
 	users := router.Group("/users")
 	auth := router.Group("/auth")
-
 	users.Use(middleware.AuthGuard())
-
 	controllers.Auth(auth)
 	controllers.Users(users)
-
 	router.Run(":4000")
 }

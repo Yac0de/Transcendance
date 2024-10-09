c43d5fbfff9c4773852f4c706ea2ad5655d65e87
Hichame Benaddi <hbenaddi@c2r1p4.42lehavre.fr>
2024-10-09 16:37:31 +0200

    probleme d installation

--- srcs/backend/main.go
+++ srcs/backend/main.go
@@ -8,7 +8,42 @@ import (
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var (
+	httpRequests = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			help: "Total number of HTTP request",
+		},
+		[]string{"method", "endpoint", "status"},
+	)
+	httpDuration = promauto.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name: "http_request_duration_seconds",
+			Help: "Duration of HTTP requests",
+		},
+		[]string{"method", "endpoint"},
+	)
+)
+
+func prometheusMiddleware() gin.HandlerFunc {
+	return func(c *gin.Default) {
+		start := time.Now()
+
+		c.Next()
+
+		duration := time.Since(start)
+		status := c.Writer.Status()
+
+		httpRequests.WithLabelValues(c.Request.Method, c.FullPath(), string(status)).Inc()
+		httpDuration.WithLabelValues(c.Request.Method, c.FullPath()).Observe(duration.Seconds())
+	}
+}
 
 func main() {
 	router := gin.Default()
@@ -24,6 +59,9 @@ func main() {
 
 	router.Use(cors.New(config))
 	router.Use(middleware.Token())
+	router.Use(prometheusMiddleware())
+	
+	router.GET("/metrics", gin.Wraph(promhttp.Handler()))
 	router.Static("/users/avatar", "./avatars")
 
 	users := router.Group("/users")

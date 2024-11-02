fc44e8b055493cfd7b6af384b4bfd50486215fc0
Omar <omar.tourabi7@gmail.com>
2024-11-02 20:23:56 +0100

    ADD: the WS server sends client status updates

---
+++ srcs/websocket/models.go
@@ -0,0 +1,22 @@
+package main
+
+type Event struct {
+	Type string `json:"type"`
+}
+
+type MessageEvent struct {
+	Event
+	Data       string `json:"data"`
+	SenderID   uint64 `json:"senderId"`
+	ReceiverID uint64 `json:"receiverId"`
+}
+
+type OnlineUsersEvent struct {
+	Event
+	Users []uint64 `json:"usersOnline"`
+}
+
+type UserStatusEvent struct {
+	Event
+	User uint64 `json:"user"`
+}

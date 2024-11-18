4eae80814fe98dd371a1432e57bb139d894f873a
Maxime Borde <maxime.borde@gmail.com>
2024-11-19 00:43:55 +0100

    ADD: Updated the front-end URLs to be HTTPS and updated CORS allowed URLS

--- srcs/frontend/src/services/apiUtils.ts
+++ srcs/frontend/src/services/apiUtils.ts
@@ -1,4 +1,6 @@
-export const API_BASE_URL = 'http://localhost:4000';
+export const API_BASE_URL = import.meta.env.PROD 
+  ? 'https://localhost:8443/api'
+  : 'http://localhost:4000' 
 
 export async function apiRequest<T>(url: string, options: RequestInit): Promise<T> {
     const response = await fetch(`${API_BASE_URL}${url}`, options);
--- srcs/frontend/src/services/websocketService.ts
+++ srcs/frontend/src/services/websocketService.ts
@@ -5,6 +5,10 @@ import { LobbyInvitationToFriend, LobbyInvitationFromFriend, LobbyAcceptFromFrie
 import { useOnlineUsersStore } from '../stores/onlineUsers';
 import { eventBus } from '../events/eventBus';
 
+const WS_URL = import.meta.env.PROD
+  ? 'wss://localhost:8443/ws'  // Production through Nginx
+  : 'ws://localhost:4001'      // Development direct access
+
 //This is needed because we can't get the return type of userStore inside the constructor of a class that is an attribute of
 //this very store, because it creates circular dependencies, so we create an interface that helps up set the return type of
 //our userStore
@@ -88,7 +92,7 @@ export class WebSocketService {
 
     public connect(): void {
         try {
-            const url = `ws://localhost:4001/ws?id=${this.clientId}`
+            const url = WS_URL + `?id=${this.clientId}`
             console.log(url);
             this.ws = new WebSocket(url);
             this.ws.onopen = () => {

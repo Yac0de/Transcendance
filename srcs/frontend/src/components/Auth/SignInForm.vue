346054d49e5c91e657d6c48bcbf1080d682628a9
Yac0de <yanis-meziane@outlook.fr>
2024-12-03 00:42:28 +0100

    ADD: Added 2fa support during sign in

--- srcs/frontend/src/components/Auth/SignInForm.vue
+++ srcs/frontend/src/components/Auth/SignInForm.vue
@@ -1,7 +1,32 @@
 <template>
-  <AuthForm formTitle="SIGN IN" submitButtonLabel="Sign In" :fields="fields" :successMessage="successMessage"
+  <AuthForm v-if="show2fa" formTitle="SIGN IN" submitButtonLabel="Sign In" :fields="fields" :successMessage="successMessage"
     :errorMessage="errorMessage" @submit="handleSubmit" @secondaryAction="handleSignup"
     secondaryButtonLabel="Sign Up" />
+  <div v-else class="confirmTwoFA">
+    <label for="confirmationCode">Please enter the confirmation code:</label>
+      <form
+        class="container-input-confirm-2fa"
+        @submit.prevent="confirm2FA"
+      >
+        <input
+          v-model="confirmationCode"
+          type="text"
+          id="confirmationCode"
+          required
+          placeholder="Enter 2FA code"
+        />
+        <input type="submit" value="Send" />
+      </form>
+      <div
+        v-if="errorMessage || successMessage"
+        :class="{
+          'error-message': errorMessage,
+          'success-message': successMessage
+        }"
+      >
+        {{ errorMessage || successMessage }}
+      </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -11,6 +36,7 @@ import api from '../../services/api';
 import AuthForm from './AuthForm.vue';
 import { useUserStore } from '../../stores/user';
 import { Field } from '../../types/models';
+import { API_BASE_URL } from '../../services/apiUtils';
 
 const nickname = ref('');
 const password = ref('');
@@ -18,6 +44,9 @@ const errorMessage = ref('');
 const successMessage = ref('');
 const router = useRouter();
 const userStore = useUserStore();
+
+const show2fa = ref<boolean>(false);
+const confirmationCode = ref('');
 
 // Form fields
 const fields: Field[] = [
@@ -41,7 +70,17 @@ const handleSubmit = async () => {
   try {
     errorMessage.value = '';
     successMessage.value = '';
-    await api.auth.signin({ nickname: nickname.value, password: password.value });
+    const response = await api.auth.signin({ nickname: nickname.value, password: password.value });
+
+        // Check if the response status is 202 (2FA required)
+    if (response.status === 202) {
+      successMessage.value = 'Two-factor authentication is required. Please enter your 2FA code.';
+
+      // Redirect to a 2FA page or show a 2FA input form
+      show2fa.value = true; // Example route for 2FA
+      return;
+    }
+    
     await userStore.fetchUser();
 
     const userId: number | null = userStore.getId;
@@ -61,6 +100,76 @@ const handleSubmit = async () => {
 const handleSignup = () => {
   router.push('/signup');
 };
+
+const resetMessages = () => {
+  errorMessage.value = '';
+  successMessage.value = '';
+};
+
+const confirm2FA = async () => {
+  resetMessages();
+  try {
+    const response = await fetch(`${API_BASE_URL}/auth/verify2FA`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
+      body: JSON.stringify({ 
+        code: confirmationCode.value,
+        nickname: nickname.value
+       }),
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to confirm 2FA code');
+    }
+
+    const result = await response.json();
+    successMessage.value = `2FA Confirmation successful: ${result.message}`;
+    await userStore.fetchUser();
+
+    const userId: number | null = userStore.getId;
+    if (userId) {
+      userStore.setWebSocketService(userId);
+    }
+    console.log('Sign in successful', userStore.getNickname);
+
+    successMessage.value = 'Sign in successful!';
+    router.push('/');
+    
+  } catch (error) {
+    console.error('Error confirming 2FA:', error);
+    errorMessage.value = "Failed to confirm 2FA. Please try again.";
+  }
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.confirmTwoFA {
+  display: flex;
+  flex-direction: column;
+  color: white;
+}
+
+.container-input-confirm-2fa {
+  display: flex;
+  justify-content: space-between;
+}
+.container-input-confirm-2fa input[type='text'] {
+  width: 100%;
+}
+.container-input-confirm-2fa input[type='submit'] {
+  width: 25%;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  color: white;
+  font-size: 14px;
+  transition: background-color 0.3s;
+  background: linear-gradient(
+    to right,
+    var(--secondary-dark-color),
+    color-mix(in srgb, var(--secondary-dark-color) 75%, white)
+  );
+}
+</style>
